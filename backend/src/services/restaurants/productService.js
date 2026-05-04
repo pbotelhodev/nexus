ba03c89435aa3ebee7392e9f9a231c6950b1240d
{ppbotelhoDev} <{ppbotelho08@gmail.com}>
2026-05-04 15:05:22 -0300

    Fix: Atualização dos campos do banco de dados

--- backend/src/services/restaurants/productService.js
+++ backend/src/services/restaurants/productService.js
@@ -6,10 +6,26 @@ const listarProdutos = async (idEmpresa) => {
       companyId: idEmpresa,
     },
   });
-
   return produtos;
 };
 
+const cadastrarProdutos = async (idEmpresa, data) => {
+  const novoProduto = await prisma.restaurantProduct.create({
+    data: {
+      name: data.name,
+      description: data.description,
+      price: data.price,
+      imageUrl: data.imageUrl,
+      category: data.category,
+      trackStock: data.trackStock,
+      stockQuantity: data.stockQuantity,
+      active: data.active,
+      
+    },
+  });
+}
+
 module.exports = {
   listarProdutos,
+  cadastrarProdutos,
 };
